ba709c5da4f76fbdeb1feb2e3a128ff1688326a4
review65 <145815372+review65@users.noreply.github.com>
2024-10-10 19:58:44 +0700

    Add files via upload

--- lib/databases/transaction_db.dart
+++ lib/databases/transaction_db.dart
@@ -1,77 +1,72 @@
 import 'dart:io';
-import 'package:account/models/kpop_band.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
-import 'package:account/models/kpop_band.dart'; // Assuming you have this model file for KpopBand
+import 'package:account/models/transactions.dart';
 
-class KpopBandDB {
+
+class TransactionDB{
   String dbName;
 
-  KpopBandDB({required this.dbName});
+  TransactionDB({required this.dbName});
 
   Future<Database> openDatabase() async {
     Directory appDirectory = await getApplicationDocumentsDirectory();
-    String dbLocation = join(appDirectory.path, dbName);
+    String dbLocation  = join(appDirectory.path, dbName);
 
     DatabaseFactory dbFactory = databaseFactoryIo;
     Database db = await dbFactory.openDatabase(dbLocation);
     return db;
   }
 
-  Future<int> insertDatabase(KpopBand band) async {
-    var db = await openDatabase();
-    var store = intMapStoreFactory.store('kpopBand');
+  Future<int> insertDatabase(Transactions statement) async{
+    var db = await this.openDatabase();
+    var store = intMapStoreFactory.store('expense');
 
-    var keyID = await store.add(db, {
-      "name": band.name,
-      "members": band.members.map((m) => {"name": m.name, "age": m.age}).toList(),
-      "debutDate": band.debutDate.toIso8601String(),
-      "albums": band.albums,  // This assumes albums is a List<String>
-      "genre": band.genre,
+    var keyID  = store.add(db, {
+      "title": statement.title,
+      "amount": statement.amount,
+      "date": statement.date.toIso8601String()
     });
-
-    db.close(); // Closing the DB after the operation
+    db.close();
     return keyID;
   }
 
-  Future<List<KpopBand>> loadAllData() async {
-    var db = await openDatabase();
-    var store = intMapStoreFactory.store('kpopBand');
-    
-    // Retrieve data from database
+  Future<List<Transactions>> loadAllData()async {
+    var db = await this.openDatabase();
+    var store = intMapStoreFactory.store('expense');
     var snapshot = await store.find(db, finder: Finder(sortOrders: [SortOrder(Field.key, false)]));
-    
-    // Convert snapshot to List of KpopBand
-    List<KpopBand> bands = [];
+    List<Transactions> transactions = [];
     for (var record in snapshot) {
-      var members = (record['members'] as List).map((m) => Member(name: m['name'], age: m['age'])).toList();
-      List<String> albums = [];
-      if (record['albums'] != null && record['albums'] is List) {
-      albums = List<String>.from(record['albums'] as List);
-      }
-      bands.add(KpopBand(
+      transactions.add(Transactions(
         keyID: record.key,
-        name: record['name'].toString(),
-        members: members,
-        debutDate: DateTime.parse(record['debutDate'].toString()),
-        albums: albums,
-        genre: record['genre'].toString(),
+        title: record['title'].toString(),
+        amount: double.parse(record['amount'].toString()),
+        date: DateTime.parse(record['date'].toString())
       ));
     }
-
-    db.close(); // Close database after fetching
-    return bands;
+    db.close();
+    return transactions;
+  }
+  deleteDatabase(int? index) async{
+    var db = await this.openDatabase();
+    var store = intMapStoreFactory.store('expense');
+    await store.delete(db, finder: Finder(filter: Filter.equals(Field.key, index)));
+    // Delete from table... where rowId = index
+    db.close();
   }
 
-  Future<void> deleteDatabase(int? index) async {
-    var db = await openDatabase();
-    var store = intMapStoreFactory.store('kpopBand');
-    
-    // Delete specific record
-    await store.delete(db, finder: Finder(filter: Filter.equals(Field.key, index)));
-    
-    db.close(); // Close DB after deletion
+  updateDatabase(Transactions statement) async{
+    var db = await this.openDatabase();
+    var store = intMapStoreFactory.store('expense');
+    var filter = Finder(filter: Filter.equals(Field.key, statement.keyID));
+    var result = store.update(db, finder: filter,  {
+      "title": statement.title,
+      "amount": statement.amount,
+      "date": statement.date.toIso8601String()
+    });
+    db.close();
+    print('update result: $result');
   }
 }
