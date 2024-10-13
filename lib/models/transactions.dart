afc2dbe4ee3f6ea8c579ee73bfab9b1ef39ddd5c
review65 <145815372+review65@users.noreply.github.com>
2024-10-13 23:26:04 +0700

    Add files via upload

--- lib/models/transactions.dart
+++ lib/models/transactions.dart
@@ -1,13 +1,39 @@
-class Transactions {
-  int? keyID;
-  final String title;
-  final double amount;
-  final DateTime date;
+class KpopBand {
+  final int? keyID;
+  final String bandName;
+  final String agency; // เพิ่มฟิลด์ agency
+  final List<Member> members;
+  final DateTime debutDate;
+  final List<String> albums;
+  final List<String> genre;
+  
 
-  Transactions({
+  KpopBand({
     this.keyID,
-    required this.title,
-    required this.amount,
-    required this.date,
+    required this.bandName,
+    required this.agency, // กำหนดเป็น required
+    required this.members,
+    required this.debutDate,
+    required this.albums,
+    required this.genre,
   });
+}
+
+
+class Member {
+  final String name;
+
+  final DateTime dob; // เปลี่ยนจาก age เป็น dob
+
+  
+
+  int get age {
+    final today = DateTime.now();
+    int age = today.year - dob.year;
+    if (today.month < dob.month || (today.month == dob.month && today.day < dob.day)) {
+      age--;
+    }
+    return age;
+  }
+  Member({required this.name, required this.dob});
 }
